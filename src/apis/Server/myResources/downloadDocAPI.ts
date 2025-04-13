1f6a2572f3d5eef54f432d26dc6c1eea1adf94b0
WuZirun <1311624739@qq.com>
2025-04-13 14:17:39 +0800

    123

--- src/apis/Server/myResources/downloadDocAPI.ts
+++ src/apis/Server/myResources/downloadDocAPI.ts
@@ -5,7 +5,7 @@ const token = localStorage.getItem('token')
 
 const downloadDocAPI = async (jsonData: string) => {
     const response = await axios.post(
-        "https://40944903.r31.cpolar.top/v1/word/export-from-json",  // 不再是 Vercel 的地址
+        "https://40944903.r31.cpolar.top/api1/v1/word/export-from-json",  // 不再是 Vercel 的地址
         jsonData,
         {
             headers: {
